ef8c591b2bc40f112f5a3458ecaddeb047bdb29c
moses ngige <moses32ngigz@gmail.com>
2022-09-26 10:31:35 +0300

    last validation error

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    # validate :clickbait?
+    validates :title , presence: true
+    validates :content , length: {minimum: 250}
+    validates :summary , length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    
+    
+
+   
+  
+    # def clickbait?
+    #     # unless CLICKBAIT_PATTERNS.include?(title)
+    #     unless ["Won't Believe","Secret","Top[number]","Guess"].any? {|word| title.include? (word)}
+    #     errors.add(:title, "must be clickbait")
+    #     end
+    # end
 end
