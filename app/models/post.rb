439ad082082cec7ab31d30f4554d5442bb699e70
moses ngige <moses32ngigz@gmail.com>
2022-09-26 14:14:57 +0300

    commit tree 4b74b825a8cd0537d270ba6be206661dd3e9c9c5

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,18 @@
 class Post < ApplicationRecord
-    # validate :clickbait?
     validates :title , presence: true
     validates :content , length: {minimum: 250}
     validates :summary , length: {maximum: 250}
     validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
-    
-    
-
+    validate :clickbait
    
   
-    # def clickbait?
-    #     # unless CLICKBAIT_PATTERNS.include?(title)
-    #     unless ["Won't Believe","Secret","Top[number]","Guess"].any? {|word| title.include? (word)}
-    #     errors.add(:title, "must be clickbait")
-    #     end
-    # end
+    def clickbait
+        unless ["You Won't Believe These True Facts", "Won't Believe","Secret","Top[number]","Guess"].include?(title)
+        #unless ["Won't Believe","Secret","Top[number]","Guess"].any? {|word| title.include? (word)}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
+
+
+
